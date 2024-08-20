16799e3656fdedd73a73e24b00d7fb66483393c8
Kukuryzka <litonisa.uwu@gmail.com>
2024-08-20 18:40:23 +0300

    end

---
+++ __astcache/@c@@users@boss@onedrive@Рабочий стол@Приют животных/c@@users@boss@onedrive@рабочий стол@приют животных@unit1.cpp
@@ -0,0 +1,22 @@
+﻿//---------------------------------------------------------------------------
+ #include <jpeg.hpp>
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Image2MouseEnter(TObject *Sender)
+{
+	Image2->Picture->LoadFromFile("BUTTON_2.jpeg");
+}
+//---------------------------------------------------------------------------
---
+++ __astcache/c@@users@boss@onedrive@Рабочий стол@Приют животных/unit1.h
@@ -0,0 +1,38 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.pngimage.hpp>
+#include <Data.DB.hpp>
+#include <Data.Win.ADODB.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *Image1;
+	TImage *Image2;
+	TEdit *Edit1;
+	TEdit *Edit2;
+	TEdit *Edit3;
+	TEdit *Edit4;
+	TADOConnection *ADOConnection1;
+	TADOQuery *ADOQuery1;
+	TDataSource *DataSource1;
+	void __fastcall Image2MouseEnter(TObject *Sender);
+	void __fastcall Image2MouseLeave(TObject *Sender);
+	void __fastcall FormActivate(TObject *Sender);
+	void __fastcall Image2Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
